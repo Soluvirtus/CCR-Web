5e6f2140ef6a48eb3073e2acb2d9f8d46568adac
miguelperezso <miguel.perezso@teampurpose.com>
2024-11-25 14:33:23 -0600

    linktree add

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
-document.addEventListener("scroll", function() {
+
+const d = document;
+const $form = d.querySelector('form');
+// Inicializar los iconos de Lucide
+lucide.createIcons();
+d.addEventListener("scroll", function () {
   const navbar = document.querySelector(".navbar");
   if (window.scrollY > 0) {
     navbar.classList.add("scrolled-navbar");
@@ -9,4 +14,39 @@ document.addEventListener("scroll", function() {
 
 
 // Agregar la clase smooth-scroll al body para la transición suave al hacer clic en los enlaces
-document.documentElement.classList.add('smooth-scroll');
+d.documentElement.classList.add('smooth-scroll');
+
+
+d.addEventListener("submit", (e) => {
+  // e.preventDefault();
+  alert("Enviando datos")
+  const $loader = d.querySelector('.contact-form-loader');
+  const $response = d.querySelector('.contact-form-response');
+  $loader.classList.remove("none");
+  // // ".form-group textarea", ".form-group button"
+  const $formInputs = d.querySelectorAll(".form-group input, textarea")
+  const $formButton = d.querySelector("#formButton")
+  
+  console.log($formButton);
+  $formButton.setAttribute("disabled", "");
+  $formInputs.forEach(element => {
+    console.log(element);
+    
+    // element.setAttribute('disabled')
+  });
+
+  setTimeout(() => {
+    $loader.classList.add("none")
+    $response.classList.remove("none");
+    $form.reset();
+    $formButton.removeAttribute("disabled");
+
+    setTimeout(() => $response.classList.add("none"), 5000);
+  }, 3000);
+})
+
+// Detectar si la URL actual es "/links"
+if (window.location.pathname === '/links') {
+  // Redirigir a linktree.html
+  window.location.replace('/linktree.html');
+}
