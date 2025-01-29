a0c9ea808e0d4b65b439ff075f5f467a98ca9f85
Antonio Avila <antonio.avila@soluvirtus.com>
2025-01-29 15:42:23 -0600

    App mode

---
+++ scripts/manifest.js
@@ -0,0 +1,21 @@
+{
+    "name": "Fundamentos de la Fe",
+    "short_name": "Fundamentos",
+    "start_url": "/fundamentos.html",
+    "display": "standalone",
+    "background_color": "#ffffff",
+    "theme_color": "#000000",
+    "icons": [
+      {
+        "src": "img/libro.ico",
+        "sizes": "48x48",
+        "type": "image/ico"
+      },
+      {
+        "src": "img/libro.ico",
+        "sizes": "48x48",
+        "type": "image/png"
+      }
+    ]
+  }
+  
